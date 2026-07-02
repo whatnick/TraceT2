156b727e45b846118e446e87e960980a4d9ecb73
Tisham (whatnick) Dhar <whatnickd@gmail.com>
2026-07-02 14:59:33 +0930

    fix: add whitenoise for static file serving in production
    - Added whitenoise middleware after SecurityMiddleware
    - Updated STORAGES to use CompressedManifestStaticFilesStorage
    - Fixed STATIC_URL to use leading slash (/static/)

--- project/settings.py
+++ project/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -155,13 +156,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "tracet/static"]
 STATIC_ROOT = BASE_DIR / "static"
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+        "BACKEND": 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     }
 }
 
